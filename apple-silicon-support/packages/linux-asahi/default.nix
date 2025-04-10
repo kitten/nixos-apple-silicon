4405f869ae491e6e3bf78eafcbee36aa8fb39aa9
Phil Pluckthun <phil@kitten.sh>
2025-04-10 08:36:32 +0100

    Bump to AsahiLinux/linux#asahi-6.14.1-2

--- apple-silicon-support/packages/linux-asahi/default.nix
+++ apple-silicon-support/packages/linux-asahi/default.nix
@@ -9,16 +9,16 @@ let
     (buildLinux rec {
       inherit stdenv lib;
 
-      version = "6.13.8-1-asahi";
-      modDirVersion = "6.13.8-asahi";
-      extraMeta.branch = "6.13";
+      version = "6.14.1-2-asahi";
+      modDirVersion = "6.14.1-asahi";
+      extraMeta.branch = "6.14";
 
       src = fetchFromGitHub {
         # tracking: https://github.com/AsahiLinux/linux/tree/asahi-wip (w/ fedora verification)
         owner = "AsahiLinux";
         repo = "linux";
-        rev = "asahi-6.13.8-1";
-        hash = "sha256-A9xcszHluBm3ehEv/yOhWzbM8bXRa7SHVrIwLp7ISi8=";
+        rev = "asahi-6.14.1-2";
+        hash = "sha256-nSKLePzFawbvaBA6BBTzNHXf9fwb56JHGLugyrSbYKY=";
       };
 
       ignoreConfigErrors = true;
