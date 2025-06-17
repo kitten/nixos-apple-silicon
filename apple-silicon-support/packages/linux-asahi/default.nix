63a0d5d37f466aabaf69c7b1d87930d8279a9002
Phil Pluckthun <phil@kitten.sh>
2025-06-17 01:21:25 +0100

    Switch back to base config

--- apple-silicon-support/packages/linux-asahi/default.nix
+++ apple-silicon-support/packages/linux-asahi/default.nix
@@ -1,12 +1,77 @@
 { lib
 , callPackage
 , linuxPackagesFor
+, writeText
 , _kernelPatches ? [ ]
 }:
 
 let
-  linux-asahi-pkg = { stdenv, lib, fetchFromGitHub, buildLinux, ... } @ args:
-    (buildLinux rec {
+  i = builtins.elemAt;
+
+  # parse <OPT> [ymn]|foo style configuration as found in a patch's extraConfig
+  # into a list of k, v tuples
+  parseExtraConfig = config:
+    let
+      lines =
+        builtins.filter (s: s != "") (lib.strings.splitString "\n" config);
+      parseLine = line: let
+        t = lib.strings.splitString " " line;
+        join = l: builtins.foldl' (a: b: "${a} ${b}")
+          (builtins.head l) (builtins.tail l);
+        v = if (builtins.length t) > 2 then join (builtins.tail t) else (i t 1);
+      in [ "CONFIG_${i t 0}" v ];
+    in map parseLine lines;
+
+  # parse <OPT>=lib.kernel.(yes|module|no)|lib.kernel.freeform "foo"
+  # style configuration as found in a patch's extraStructuredConfig into
+  # a list of k, v tuples
+  parseExtraStructuredConfig = config: lib.attrsets.mapAttrsToList
+    (k: v: [ "CONFIG_${k}" (v.tristate or v.freeform) ] ) config;
+
+  parsePatchConfig = { extraConfig ? "", extraStructuredConfig ? {}, ... }:
+    (parseExtraConfig extraConfig) ++
+    (parseExtraStructuredConfig extraStructuredConfig);
+
+  # parse CONFIG_<OPT>=[ymn]|"foo" style configuration as found in a config file
+  # into a list of k, v tuples
+  parseConfig = config:
+    let
+      parseLine = builtins.match ''(CONFIG_[[:upper:][:digit:]_]+)=(([ymn])|"([^"]*)")'';
+      # get either the [ymn] option or the "foo" option; whichever matched
+      t = l: let v = (i l 2); in [ (i l 0) (if v != null then v else (i l 3)) ];
+      lines = lib.strings.splitString "\n" config;
+    in map t (builtins.filter (l: l != null) (map parseLine lines));
+
+  origConfigfile = ./config;
+
+  linux-asahi-pkg = { stdenv, lib, fetchFromGitHub, linuxKernel, ... } @ args:
+    let
+      origConfigText = builtins.readFile origConfigfile;
+
+      # extraConfig from all patches in order
+      extraConfig =
+        lib.fold (patch: ex: ex ++ (parsePatchConfig patch)) [] _kernelPatches;
+      # config file text for above
+      extraConfigText = let
+        text = k: v: if (v == "y") || (v == "m") || (v == "n")
+          then "${k}=${v}" else ''${k}="${v}"'';
+      in (map (t: text (i t 0) (i t 1)) extraConfig);
+
+      # final config as a text file path
+      configfile = if extraConfig == [] then origConfigfile else
+        writeText "config" ''
+          ${origConfigText}
+
+          # Patches
+          ${lib.strings.concatStringsSep "\n" extraConfigText}
+        '';
+      # final config as an attrset
+      configAttrs = let
+        makePair = t: lib.nameValuePair (i t 0) (i t 1);
+        configList = (parseConfig origConfigText) ++ extraConfig;
+      in builtins.listToAttrs (map makePair (lib.lists.reverseList configList));
+    in
+    (linuxKernel.manualConfig rec {
       inherit stdenv lib;
 
       version = "6.14.8-1-asahi";
@@ -17,26 +82,15 @@ let
         # tracking: https://github.com/AsahiLinux/linux/tree/asahi-wip (w/ fedora verification)
         owner = "AsahiLinux";
         repo = "linux";
-        rev = "asahi-6.14.8-1";
+        rev = version;
         hash = "sha256-JrWVw1FiF9LYMiOPm0QI0bg/CrZAMSSVcs4AWNDIH3Q=";
       };
-
-      ignoreConfigErrors = true;
 
       kernelPatches = [
         {
           name = "Asahi config";
           patch = null;
           extraStructuredConfig = with lib.kernel; {
-            # Slim down common config
-            STAGING = lib.mkForce no;
-            MICROCODE = lib.mkForce no;
-            VIRT_DRIVERS = lib.mkForce no;
-            XEN = lib.mkForce unset;
-            MEDIA_DIGITAL_TV_SUPPORT = lib.mkForce no;
-            MEDIA_ANALOG_TV_SUPPORT = lib.mkForce no;
-            CHROME_PLATFORMS = lib.mkForce no;
-
             # Enforced Config
             # See: https://github.com/AsahiLinux/docs/blob/28f210d1e357d649d2e60ab908714ac3cb7da538/docs/Kernel-config-notes-for-distros.md
             DRM = yes;
@@ -46,60 +100,6 @@ let
             RUST_DEBUG_ASSERTIONS = unset;
             RUST_OVERFLOW_CHECKS = yes;
             RUST_BUILD_ASSERT_ALLOW = unset;
-
-            UCLAMP_TASK = yes;
-            UCLAMP_TASK_GROUP = yes;
-            SUSPEND = yes;
-            HIBERNATION = unset;
-            CPU_FREQ_DEFAULT_GOV_SCHEDUTIL = yes;
-            CPU_FREQ_DEFAULT_GOV_PERFORMANCE = lib.mkForce unset;
-            ENERGY_MODEL = yes;
-            SERIAL_SAMSUNG = yes;
-            SERIAL_SAMSUNG_CONSOLE = yes;
-            REGULATOR_FIXED_VOLTAGE = yes;
-            WATCHDOG_HANDLE_BOOT_ENABLED = yes;
-            DRM_ASAHI_DEBUG_ALLOCATOR = unset;
-            DRM_FBDEV_EMULATION = yes;
-            DRM_SCHED = yes;
-            DRM_VGEM = no;
-            DRM_GEM_SHMEM_HELPER = yes;
-            SND_SOC_CS42L83 = module;
-            SND_SOC_TAS2764 = module;
-            SND_SOC_TAS2770 = module;
-            HID_BATTERY_STRENGTH = yes;
-            HID_APPLE = module;
-            HID_MAGICMOUSE = module;
-            MOUSE_APPLETOUCH = module;
-            INPUT_LEDS = yes;
-            LEDS_PWM = yes;
-            USB_XHCI_HCD = module;
-            USB_DWC3 = module;
-            USB_DWC3_DUAL_ROLE = yes;
-            TYPEC_TPS6598X = module;
-            MMC_SDHCI = module;
-            MMC_SDHCI_PCI = module;
-            NET_VENDOR_AQUANTIA = yes;
-            AQTION = module;
-            NET_VENDOR_BROADCOM = yes;
-            TIGON3 = module;
-            BT = module;
-            BT_BREDR = yes;
-            BT_RFCOMM = module;
-            BT_BNEP = module;
-            BT_HIDP = module;
-            BT_LE = yes;
-            BT_HCIUART_BCM = yes;
-            CFG80211 = module;
-            WLAN_VENDOR_BROADCOM = yes;
-            BRCMFMAC = module;
-            BRCMFMAC_PROTO_BCDC = yes;
-            BRCMFMAC_PROTO_MSGBUF = yes;
-            BRCMFMAC_USB = yes;
-            BRCMFMAC_PCIE = yes;
-            APPLE_MFI_FASTCHARGE = module;
-            SND_SOC = yes;
-            SND_SOC_GENERIC_DMAENGINE_PCM = module;
-            SND_SOC_COMPRESS = yes;
 
             # Asahi Config
             # See: https://github.com/AsahiLinux/docs/blob/28f210d1e357d649d2e60ab908714ac3cb7da538/docs/Kernel-config-notes-for-distros.md
@@ -174,135 +174,6 @@ let
             DRM_PANEL = yes;
             DRM_ACCEL = yes;
 
-            # Explicit sound overrides
-            SOUND_OSS_CORE = yes;
-            SOUND_OSS_CORE_PRECLAIM = yes;
-            SND_PCM = yes;
-            SND_DMAENGINE_PCM = module;
-            SND_HWDEP = module;
-            SND_SEQ_DEVICE = yes;
-            SND_COMPRESS_OFFLOAD = yes;
-            SND_OSSEMUL = yes;
-            SND_MIXER_OSS = module;
-            SND_PCM_OSS = module;
-            SND_PCM_OSS_PLUGINS = yes;
-            SND_PCM_TIMER = yes;
-            SND_HRTIMER = yes;
-            SND_DYNAMIC_MINORS = yes;
-            SND_PROC_FS = yes;
-            SND_VERBOSE_PROCFS = yes;
-            SND_CTL_FAST_LOOKUP = yes;
-            SND_CTL_INPUT_VALIDATION = yes;
-            SND_UTIMER = yes;
-            SND_VMASTER = yes;
-            SND_SEQUENCER = yes;
-            SND_SEQ_DUMMY = yes;
-            SND_SEQUENCER_OSS = module;
-            SND_SEQ_HRTIMER_DEFAULT = yes;
-            SND_SEQ_MIDI_EVENT = module;
-            SND_SEQ_MIDI = module;
-            SND_SEQ_VIRMIDI = module;
-            SND_MPU401_UART = module;
-            SND_DRIVERS = yes;
-            SND_DUMMY = module;
-            SND_ALOOP = module;
-            SND_VIRMIDI = module;
-            SND_MTPAV = module;
-            SND_SERIAL_U16550 = module;
-            SND_MPU401 = module;
-
-            # Skip some DRM modules
-            DRM_RADEON = lib.mkForce no;
-            DRM_AMDGPU = lib.mkForce no;
-            DRM_NOUVEAU = lib.mkForce no;
-            DRM_XE = lib.mkForce no;
-            DRM_VKMS = lib.mkForce no;
-            DRM_VMWGFX = lib.mkForce no;
-            DRM_UDL = module;
-            DRM_AST = lib.mkForce no;
-            DRM_MGAG200 = lib.mkForce no;
-            DRM_QXL = lib.mkForce no;
-            # Associated unsets
-            DRM_AMDGPU_CIK = lib.mkForce unset;
-            DRM_AMDGPU_SI = lib.mkForce unset;
-            DRM_AMDGPU_USERPTR = lib.mkForce unset;
-            DRM_AMD_ACP = lib.mkForce unset;
-            DRM_AMD_DC_FP = lib.mkForce unset;
-            DRM_AMD_DC_SI = lib.mkForce unset;
-            DRM_AMD_ISP = lib.mkForce unset;
-            DRM_AMD_SECURE_DISPLAY = lib.mkForce unset;
-            DRM_NOUVEAU_GSP_DEFAULT = lib.mkForce unset;
-            DRM_NOUVEAU_SVM = lib.mkForce unset;
-            HSA_AMD = lib.mkForce unset;
-
-            # Skip some networking modules
-            WLAN_VENDOR_INTEL = lib.mkForce no;
-            WLAN_VENDOR_INTERSIL = lib.mkForce no;
-            WLAN_VENDOR_MARVELL = lib.mkForce no;
-            WCN36XX = lib.mkForce no;
-            ATH11K = lib.mkForce no;
-            ATH12K = lib.mkForce no;
-            ATH5K = lib.mkForce no;
-            ATH5K_PCI = lib.mkForce unset;
-            ATH9K = lib.mkForce no;
-            NET_VENDOR_MEDIATEK = lib.mkForce unset;
-
-            # Skip some Crypto modules
-            OCTEONTX2_AF = lib.mkForce no;
-            OCTEONTX2_PF = lib.mkForce no;
-            CRYPTO_DEV_MARVELL_CESA = lib.mkForce unset;
-
-            # Explicitly disable unsupported architectures
-            ARCH_ACTIONS = lib.mkForce no;
-            ARCH_AIROHA = lib.mkForce no;
-            ARCH_SUNXI = lib.mkForce no;
-            ARCH_ALPINE = lib.mkForce no;
-            ARCH_BCM = lib.mkForce no;
-            ARCH_BERLIN = lib.mkForce no;
-            ARCH_BITMAIN = lib.mkForce no;
-            ARCH_EXYNOS = lib.mkForce no;
-            ARCH_SPARX5 = lib.mkForce no;
-            ARCH_K3 = lib.mkForce no;
-            ARCH_LG1K = lib.mkForce no;
-            ARCH_HISI = lib.mkForce no;
-            ARCH_KEEMBAY = lib.mkForce no;
-            ARCH_MEDIATEK = lib.mkForce no;
-            ARCH_MESON = lib.mkForce no;
-            ARCH_MVEBU = lib.mkForce no;
-            ARCH_NXP = lib.mkForce no;
-            ARCH_MA35 = lib.mkForce no;
-            ARCH_NPCM = lib.mkForce no;
-            ARCH_PENSANDO = lib.mkForce no;
-            ARCH_QCOM = lib.mkForce no;
-            ARCH_REALTEK = lib.mkForce no;
-            ARCH_RENESAS = lib.mkForce no;
-            ARCH_ROCKCHIP = lib.mkForce no;
-            ARCH_SEATTLE = lib.mkForce no;
-            ARCH_INTEL_SOCFPGA = lib.mkForce no;
-            ARCH_STM32 = lib.mkForce no;
-            ARCH_SYNQUACER = lib.mkForce no;
-            ARCH_TEGRA = lib.mkForce no;
-            ARCH_SPRD = lib.mkForce no;
-            ARCH_THUNDER = lib.mkForce no;
-            ARCH_THUNDER2 = lib.mkForce no;
-            ARCH_UNIPHIER = lib.mkForce no;
-            ARCH_VEXPRESS = lib.mkForce no;
-            ARCH_VISCONTI = lib.mkForce no;
-            ARCH_XGENE = lib.mkForce no;
-            ARCH_ZYNQMP = lib.mkForce no;
-
-            # IOMMU modules
-            IOMMU_IOVA = yes;
-            IOMMU_API = yes;
-            IOMMU_SUPPORT = yes;
-            IOMMU_IO_PGTABLE = yes;
-            IOMMU_IO_PGTABLE_LPAE = yes;
-            IOMMU_IO_PGTABLE_ARMV7S = yes;
-            IOMMU_IO_PGTABLE_DART = yes;
-            IOMMU_DEFAULT_DMA_STRICT = yes;
-            OF_IOMMU = yes;
-            IOMMU_DMA = yes;
-
             # Explicit custom overrides
             NVME_AUTH = lib.mkForce yes;
             HZ_1000 = yes;
@@ -312,6 +183,9 @@ let
           features.rust = true;
         }
       ] ++ _kernelPatches;
+
+      inherit configfile;
+      config = configAttrs;
     } // (args.argsOverride or {})).overrideAttrs (old: {
       NIX_CFLAGS_COMPILE = (old.NIX_CFLAGS_COMPILE or "") + " -march=armv8.6-a+fp16+fp16fml+aes+sha2+sha3+bf16+i8mm+nosve+nosve2+nomemtag+nosm4+nof32mm+nof64mm";
       hardeningEnable = [ "pic" "format" "fortify" "stackprotector" ];
